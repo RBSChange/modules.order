fb31e4cecd25125f2a0953cf26ba37630e0c1779
Gaël PORT <gael.port@rbs.fr>
2010-08-03 18:04:23 +0200

    [NEW] Filter on order containing a product product with a given brand.

---
+++ persistentdocument/filters/OrdersContainingBrandFilter.php
@@ -0,0 +1,32 @@
+<?php
+class order_OrdersContainingBrandFilter extends f_persistentdocument_DocumentFilterImpl
+{
+	public function __construct()
+	{
+		$info = new BeanPropertyInfoImpl('brand', 'modules_brand/brand');
+		$info->setMaxOccurs(-1);
+		$info->setMinOccurs(1);
+		$parameters['brand'] = new f_persistentdocument_DocumentFilterValueParameter($info);
+		$this->setParameters($parameters);
+	}
+	
+	/**
+	 * @return String
+	 */
+	public static function getDocumentModelName()
+	{
+		return 'modules_order/order';
+	}
+	
+	/**
+	 * @return f_persistentdocument_criteria_Query
+	 */
+	public function getQuery()
+	{
+		$query = order_OrderService::getInstance()->createQuery();
+		$criteria1 = $query->createCriteria('line');
+		$criteria2 = $criteria1->createPropertyCriteria('productId', 'modules_catalog/product');
+		$criteria2->add(Restrictions::in('brand', $this->getParameter('brand')->getValueForQuery()));
+		return $query;
+	}
+}
