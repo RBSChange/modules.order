d1b6b28acfca282f10df431e1ff0daef85bf446c
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-02-23 12:06:25 +0100

    [FIX #27404] La génération asynchrone des expéditions peut donner l'impression que les expéditions ne fonctionnent pas

--- actions/LoadOrderShippingAction.class.php
+++ actions/LoadOrderShippingAction.class.php
@@ -12,6 +12,7 @@ class order_LoadOrderShippingAction extends generic_LoadJSONAction
 	public function _execute($context, $request)
 	{
 		$order = $this->getDocumentInstanceFromRequest($request);
+		order_ModuleService::getInstance()->checkOrderProcessing($order);
 		$data = $this->exportFieldsData($order, array('shipping'));
 		return $this->sendJSON($data);
 	}
