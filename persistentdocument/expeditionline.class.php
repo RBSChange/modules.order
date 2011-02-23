e4ef54fa9f9801b6c441db6ad328deda7fda54db
Gaël PORT <gael.port@rbs.fr>
2011-02-23 10:01:33 +0100

    [REFACTOR] Move addTreeAttributes and addFormProperties from final document class to document service.

--- persistentdocument/expeditionline.class.php
+++ persistentdocument/expeditionline.class.php
@@ -6,23 +6,6 @@
 class order_persistentdocument_expeditionline extends order_persistentdocument_expeditionlinebase 
 {
 	/**
-	 * @param string $moduleName
-	 * @param string $treeType
-	 * @param array<string, string> $nodeAttributes
-	 */
-//	protected function addTreeAttributes($moduleName, $treeType, &$nodeAttributes)
-//	{
-//	}
-	
-	/**
-	 * @param string $actionType
-	 * @param array $formProperties
-	 */
-//	public function addFormProperties($propertiesNames, &$formProperties)
-//	{	
-//	}
-
-	/**
 	 * @return order_persistentdocument_orderline
 	 */
 	private function getOrderLine()
