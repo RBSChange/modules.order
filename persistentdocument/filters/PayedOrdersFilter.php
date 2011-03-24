f08aa3942f94f109bfc31625d69a02145f5ec94c
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-03-24 18:29:32 +0100

    [EVO#28208] Filtre de commandes payé

---
+++ persistentdocument/filters/PayedOrdersFilter.php
@@ -0,0 +1,26 @@
+<?php
+class order_PayedOrdersFilter extends f_persistentdocument_DocumentFilterImpl
+{
+	public function __construct()
+	{
+	}
+	
+	/**
+	 * @return String
+	 */
+	public static function getDocumentModelName()
+	{
+		return 'modules_order/order';
+	}
+	
+	/**
+	 * @return f_persistentdocument_criteria_Query
+	 */
+	public function getQuery()
+	{
+		$orderQuery = order_OrderService::getInstance()->createQuery();
+		$orderQuery->createCriteria("bill")
+			->add(Restrictions::eq("status", order_BillService::SUCCESS));
+		return $orderQuery;
+	}
+}
