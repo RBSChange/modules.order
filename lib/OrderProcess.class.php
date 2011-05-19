9f32df7efaa242e01c012067c512e16f01434c02
Gaël PORT <gael.port@rbs.fr>
2011-05-19 17:29:24 +0200

    Some cleanings.

--- lib/OrderProcess.class.php
+++ lib/OrderProcess.class.php
@@ -5,18 +5,17 @@ class order_OrderProcess
 	 * @var string
 	 */
 	protected $currentStep;
-	
+
 	/**
 	 * @var string
 	 */
 	protected $startStep = 'Identify';
-	
+
 	/**
 	 * @var string
-	 */	
+	 */
 	protected $lastStep = 'Payment';
-	
-	
+
 	/**
 	 * @var array
 	 */
@@ -27,14 +26,14 @@ class order_OrderProcess
 		'Confirm' => array('blocType' => 'order_ConfirmStep', 'nextStep' => 'Payment'),
 		'Payment' => array('blocType' => 'order_PaymentStep', 'nextStep' => null),
 	);
-	
+
 	public function __construct()
 	{
 		$steps = array_keys($this->config);
 		$this->startStep = f_util_ArrayUtils::firstElement($steps);
 		$this->lastStep = f_util_ArrayUtils::lastElement($steps);
 	}
-			
+		
 	/**
 	 * @param string $step
 	 * @return string | null;
@@ -46,8 +45,8 @@ class order_OrderProcess
 			return $this->config[$step]['blocType'];
 		}
 		return null;
-	}	
-	
+	}
+
 	/**
 	 * @param string $step
 	 * @return string | null;
@@ -68,15 +67,15 @@ class order_OrderProcess
 	{
 		return $this->currentStep ? $this->currentStep : $this->getFirstStep();
 	}
-	
+
 	/**
 	 * @return boolean
 	 */
 	public function inProcess()
 	{
 		return ($this->currentStep !== null && $this->currentStep !== $this->getLastStep());
-	}	
-	
+	}
+
 	/**
 	 * @return string
 	 */
@@ -84,7 +83,7 @@ class order_OrderProcess
 	{
 		return $this->getBlockTypeForStep($this->getCurrentStep());
 	}
-	
+
 	/**
 	 * @param string $step
 	 */
@@ -102,29 +101,33 @@ class order_OrderProcess
 
 	/**
 	 * @return string
-	 */	
+	 */
 	public function getFirstStep()
 	{
 		return $this->startStep;
 	}
-	
+
 	/**
 	 * @return string
-	 */	
+	 */
 	public function getLastStep()
 	{
 		return $this->lastStep;
 	}
-		
+
+	/**
+	 * @return array
+	 */
 	public function getSteps()
 	{
 		$result = array();
 		$name = $this->startStep;
 		$hasURL = true;
 		$step = 1;
-		while ($name !== null) 
+		$ls = LocaleService::getInstance();
+		while ($name !== null)
 		{
-			$result[$name] = array('name' => $name, 'step' => $step, 'current'=> false, 'label' => f_Locale::translate('&modules.order.process.' . ucfirst($name) . ';'));
+			$result[$name] = array('name' => $name, 'step' => $step, 'current'=> false, 'label' => $ls->transFO('m.order.process.' . $name, array('ucf')));
 			if ($name == $this->getCurrentStep())
 			{
 				$result[$name]['current'] = true;
@@ -148,7 +151,7 @@ class order_OrderProcess
 	{
 		return order_OrderProcessService::getInstance()->getStepURL($step, $this);
 	}
-	
+
 	/**
 	 * @return string || null
 	 */
@@ -156,9 +159,11 @@ class order_OrderProcess
 	{
 		return order_OrderProcessService::getInstance()->getOrderProcessURL($this);
 	}
-	
+
+	// Deprecated.
+
 	/**
-	 * @deprecated use order_OrderProcessService::getInstance()->loadFromSession()
+	 * @deprecated (will be removed in 4.0) use order_OrderProcessService::getInstance()->loadFromSession()
 	 */
 	public static function getInstance()
 	{
