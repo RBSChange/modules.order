79d14b303aa0e4a2bcbc2f4b332832afd0ba759a
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-06-13 12:12:27 +0200

    [FIX #34647] date GMT

--- persistentdocument/message.class.php
+++ persistentdocument/message.class.php
@@ -12,16 +12,10 @@ class order_persistentdocument_message extends order_persistentdocument_messageb
 	/**
 	 * Get the readable message date.
 	 * @return String
+	 * @deprecated use getCreationdate or getUICreationdate
 	 */
 	public function getMessageDate()
 	{
-		if (defined('DEFAULT_TIMEZONE'))
-		{
-			date_default_timezone_set(DEFAULT_TIMEZONE);        
-		}
-		$offset = intval(date('Z'));
-		$creationDate = date_Calendar::getInstance($this->getCreationdate());
-		$creationDate->add(date_Calendar::SECOND, $offset);
-		return $creationDate->toString();
+		return $this->getCreationdate();
 	}
 }
