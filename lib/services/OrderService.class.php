7beb5f5be36bf78b75fc1c5ea7808f374984122a
Gaël PORT <gael.port@rbs.fr>
2011-09-16 11:39:28 +0200

    [FIX #42365] Ligne de commande et déclinaison

--- lib/services/OrderService.class.php
+++ lib/services/OrderService.class.php
@@ -172,15 +172,7 @@ class order_OrderService extends f_persistentdocument_DocumentService
 	{
 		$lineInfo = array();
 		$lineInfo['linetype'] = $type;
-		$product = $line->getProduct();
-		if ($product === null)
-		{
-			$lineInfo['productLabel'] = f_Locale::translateUI('&module.customer.bo.doceditor.panel.carts.Unexisting-product;');
-		}
-		else
-		{
-			$lineInfo['productLabel'] = $product->getLabel();
-		}
+		$lineInfo['productLabel'] = $line->getOrderLabel() ? $line->getOrderLabel() : $line->getLabel();
 		$lineInfo['codeReference'] = $line->getCodeReference();
 
 		$lineInfo['unitPriceWithoutTax'] = $order->formatPrice($line->getUnitPriceWithoutTax());
