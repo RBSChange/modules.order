94a3e4e1c9b90a6811aa4a01d36c2bef498508ff
Loïc Couturier <loic.couturier@rbs.fr>
2012-04-24 17:56:01 +0200

    [UPDATE] Change folder label to use locale key

--- persistentdocument/waitingresponseorderfolder.class.php
+++ persistentdocument/waitingresponseorderfolder.class.php
@@ -4,11 +4,4 @@
  */
 class order_persistentdocument_waitingresponseorderfolder extends order_persistentdocument_waitingresponseorderfolderbase 
 {
-	/**
-	 * @return string
-	 */
-	public function getLabel()
-	{
-		return LocaleService::getInstance()->transBO('m.order.document.waitingresponseorderfolder.document-name', array('ucf'));
-	}
 }
