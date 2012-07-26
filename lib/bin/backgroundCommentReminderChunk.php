1353916da63b8491fc47a172c101e66719908cb9
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-07-26 09:03:15 +0200

    [FIX #62382] Consommation excessive de mémoire de order_BackgroundCommentReminder

--- lib/bin/backgroundCommentReminderChunk.php
+++ lib/bin/backgroundCommentReminderChunk.php
@@ -3,20 +3,23 @@ echo "id:-1";
 $lastId = $argv[0];
 $chunkSize = $argv[1];
 $tm = f_persistentdocument_TransactionManager::getInstance();
-try
+$oos = order_OrderService::getInstance();
+$ids = $oos->getOrderIdsToRemind($lastId, $chunkSize);
+foreach (array_chunk($ids, 10) as $chunk)
 {
-	$tm->beginTransaction();
-	$oos = order_OrderService::getInstance();
-	$ids = $oos->getOrderIdsToRemind($lastId, $chunkSize);
-	foreach ($ids as $id)
+	try
 	{
-		echo PHP_EOL, 'id:', $id;
-		$oos->sendCommentReminder(order_persistentdocument_order::getInstanceById($id));
+		$tm->beginTransaction();	
+		foreach ($chunk as $id)
+		{
+			echo PHP_EOL, 'id:', $id;
+			$oos->sendCommentReminder(order_persistentdocument_order::getInstanceById($id));
+		}
+		$tm->commit();
 	}
-	$tm->commit();
-}
-catch (Exception $e)
-{
-	echo PHP_EOL, 'EXCEPTION: ', $e->getMessage();
-	$tm->rollBack($e);
+	catch (Exception $e)
+	{
+		echo PHP_EOL, 'EXCEPTION: ', $e->getMessage();
+		$tm->rollBack($e);
+	}
 }
