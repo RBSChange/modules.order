4178fc098c04897a7e201a432feda98e4ceff046
Loïc Couturier <loic.couturier@rbs.fr>
2012-12-12 15:26:20 +0100

    [UPDATE] Add method on FeesScriptDocumentElement to permit to import Fee strategy

--- persistentdocument/import/FeesScriptDocumentElement.class.php
+++ persistentdocument/import/FeesScriptDocumentElement.class.php
@@ -20,4 +20,22 @@ class order_FeesScriptDocumentElement extends import_ScriptDocumentElement
 	{
 		return f_persistentdocument_PersistentDocumentModel::getInstanceFromDocumentModelName('modules_order/fees');
 	}
+	
+	/**
+	 * @param import_ScriptExecuteElement $scriptExecute
+	 * @param array<string, string> $attr
+	 * @throws Exception
+	 */
+	public function setStrategyParam($scriptExecute, $attr = null)
+	{
+	
+		if (!is_array($attr) || !array_key_exists('name', $attr) || !array_key_exists('value', $attr))
+		{
+			throw new Exception(__METHOD__ . ' need params name and value');
+		}
+	
+		$this->getPersistentDocument()->setStrategyParam($attr['name'], $attr['value']);
+	
+		$this->saveDocument();
+	}
 }
